8e4d1b1e5bc83bfa3e6cf964361658f9c904e2bb
Eliu Hepher G. <egutierrez@ia.com.mx>
2024-05-04 03:51:07 -0600

    refactor: :art: Code cleaning up

--- api/views.py
+++ api/views.py
@@ -1,5 +1,3 @@
-from typing import Any
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -14,23 +12,26 @@ from .serializers import (
     ProductRequestSerializer,
     ProductResponseSerializer,
     ProductPatchSerializer,
-    ProductPatchRequestSerializer,
-    OrderSerializer,
 )
 
-from .models import Product, Order
+from .models import Product
 
 
 class ProductView(APIView):
+    """
+    Views to handle Product requests
+    """
 
-    def get(self, request, sku=None):
+    def get(self, _request, sku=None):
 
         if sku:
             try:
                 product = Product.objects.get(sku=sku)
                 serializer = ProductResponseSerializer(product)
             except Product.DoesNotExist as error:
-                return Response({"error":True, "message": error}, status=HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": True, "message": error}, status=HTTP_400_BAD_REQUEST
+                )
 
         else:
             products = Product.objects.all()
@@ -52,9 +53,10 @@ class ProductView(APIView):
                 product_to_update = Product.objects.get(sku=sku)
             except Product.DoesNotExist as error:
                 return Response(
-                    data={"error": True, "message": f"{error}"}, status=HTTP_404_NOT_FOUND
+                    data={"error": True, "message": f"{error}"},
+                    status=HTTP_404_NOT_FOUND,
                 )
-            
+
             serializer = ProductPatchSerializer(
                 product_to_update,
                 data={"stock": product_to_update.stock + updated_stock},
@@ -65,7 +67,13 @@ class ProductView(APIView):
                 return Response(serializer.data, HTTP_200_OK)
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
         else:
-            return Response(data={"message":"The field 'stock' is required in request body", "error":True}, status=HTTP_400_BAD_REQUEST)
+            return Response(
+                data={
+                    "message": "The field 'stock' is required in request body",
+                    "error": True,
+                },
+                status=HTTP_400_BAD_REQUEST,
+            )
 
 
 class OrderView(APIView):
@@ -77,27 +85,30 @@ class OrderView(APIView):
         for element in data:
             sku = element.get("sku")
             purchase = element.get("quantity")
-            all_have_stock.append(True if purchase <= Product.objects.get(sku=sku).stock else False)
+            all_have_stock.append(
+                True if purchase <= Product.objects.get(sku=sku).stock else False
+            )
 
         if all(all_have_stock):
             for element in data:
-                print(element)
+
                 sku = element.get("sku")
                 purchase = element.get("quantity")
 
                 product_to_update = Product.objects.get(sku=sku)
+                if product_to_update.stock <= 10:
+                    print(f"¡Atención! El producto {product_to_update.sku} | {product_to_update.name} está próximo a agotarse. Su stock actual es de {product_to_update.stock}")
                 product_to_update.stock = product_to_update.stock - purchase
                 product_to_update.save()
-                #order = Order.objects.create(total=1, sales_channel="API Client")
 
-                ## TODO
-                # enviar correo cuando el stock sea menor a 10
-                # enviar correo cuando se intente comprar una cantidad mayor al stock
-            
-            return Response(data={"message":"Compra realizada con éxito", "error":False}, status=HTTP_200_OK) 
-        
+                
+            return Response(
+                data={"message": "Compra realizada con éxito", "error": False},
+                status=HTTP_200_OK,
+            )
+
         else:
             return Response(
-                data={"message":"No se pudo concretar la compra", "error":False},
+                data={"message": "No se pudo concretar la compra", "error": False},
                 status=HTTP_400_BAD_REQUEST,
             )
